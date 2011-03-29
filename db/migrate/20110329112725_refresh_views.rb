db841a911ec39edacf5a0e6f29669fe97ec7797f
andrewjpage <andrewjpage@gmail.com>
2011-03-29 12:33:59 +0100

    refresh views

---
+++ db/migrate/20110329112725_refresh_views.rb
@@ -0,0 +1,22 @@
+class RefreshViews < ActiveRecord::Migration
+
+  def self.up
+    table_names = %w[asset_freezers batch_requests batches billing_events events lanes library_tubes multiplexed_library_tubes 
+        plate_purposes plates projects quotas requests sample_tubes samples studies study_samples tag_instances tags wells]
+
+    newtable_names = %w[asset_audits pulldown_multiplexed_library_tubes]
+    
+    table_names.each do |table_name|
+      execute("drop view current_#{table_name};")
+    end
+    
+    all_tables = (table_names | newtable_names).flatten
+    all_tables.each do |table_name|
+      execute("create view current_#{table_name} as select * from #{table_name} where is_current=1;")
+    end
+    
+  end
+
+  def self.down
+  end
+end
