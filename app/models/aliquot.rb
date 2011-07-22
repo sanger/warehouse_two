b611885583773812ebfa188044907e2704166917
Maxime Bourget <mb14@sanger.ac.uk>
2011-07-22 12:36:49 +0100

    basic test pass for aliquot with all fields

--- app/models/aliquot.rb
+++ app/models/aliquot.rb
@@ -14,15 +14,23 @@ class Aliquot < ActiveRecord::Base
       :receptacle_internal_id               => :receptacle_internal_id,
       :receptacle_type                      => :receptacle_type,
       :sample_uuid                          => :sample_uuid,
-      :sample_internal_id                   => :sample_internal_i,
+      :sample_internal_id                   => :sample_internal_id,
+      :study_uuid                           => :study_uuid,
+      :study_internal_id                    => :study_internal_id,
       :project_uuid                         => :project_uuid,
-      :project_internal_id                  => :project_internal_i,
+      :project_internal_id                  => :project_internal_id,
+      :receptacle_internal_id               => :receptacle_internal_id,
+      :receptacle_uuid                      => :receptacle_uuid,
+      :receptacle_type                      => :receptacle_type,
       :tag_uuid                             => :tag_uuid,
       :tag_internal_id                      => :tag_internal_id,
+      :library_uuid                         => :library_uuid,
+      :library_internal_id                  => :library_internal_id,
+      :library_type                         => :library_type,
       :insert_size_from                     => :insert_size_from,
       :insert_size_to                       => :insert_size_to,
       :last_updated                         => :updated_at,
-      :created                              => :created_a
+      :created                               => :created_at
     }
   end
 
