04cf7af2b0dcd6a2429d957f08c2efb4d80a9e9f
Matthew Denner <md12@sanger.ac.uk>
2012-03-14 13:47:32 +0000

    Fixed project
    Ignore manager & follower arrays in the project JSON

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   include ResourceTools
 
   json do
+    ignore(:manager, :follower)
     translate(:id => :internal_id)
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Project do
   it_behaves_like 'a resource'
   it_behaves_like 'maps JSON fields', :id => :internal_id
+  it_behaves_like 'ignores JSON fields', [ :manager, :follower ]
 
   let(:json) do
     {
@@ -19,6 +20,8 @@ describe Project do
       :project_manager         => "project manager",
       :budget_cost_centre      => "budget cost centre",
       :state                   => "state",
+      :manager                 => [ { :name => "manager name",  :email => "manager email",  :login => "manager login"  } ],
+      :follower                => [ { :name => "follower name", :email => "follower email", :login => "follower login" } ],
       :updated_at              => "2012-03-11 10:27:30",
       :created_at              => "2012-03-11 10:27:30"
     }
