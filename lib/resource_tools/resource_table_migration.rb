d57cf26227859c5e3887b306fda175e899ca9f95
Matthew Denner <matt.denner@gmail.com>
2012-03-31 10:16:19 +0100

    is_current index drop & uuid_objects update
    - Dropped is_current indexes as they are redundant
    - Updated uuid_objects to use current_to rather than is_current

---
+++ lib/resource_tools/resource_table_migration.rb
@@ -0,0 +1,14 @@
+module ResourceTools::ResourceTableMigration
+  def each_resource_table(&block)
+    connection.tables.each do |table|
+      next if table =~ /^current_/ or [ 'asset_freezers' ].include?(table)
+      yield(table) if ['current_from','current_to'].all? { |c| has_column?(table, c) }
+    end
+  end
+  private :each_resource_table
+
+  def has_column?(table, column)
+    connection.columns(table).any? { |c| column.to_s == c.name.to_s }
+  end
+  private :has_column?
+end
