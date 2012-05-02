641b77835276200cc2016382eeb9175f3abb9d95
Matthew Denner <matt.denner@gmail.com>
2012-05-02 19:17:28 +0100

    [#28956849] Automatically generate triggers on table change
    Whenever the resource tables are changed through change_resource_table
    the trigger will be appropriately updated so that they should work
    without causing deadlocks.

--- lib/resource_tools/resource_table_migration.rb
+++ lib/resource_tools/resource_table_migration.rb
@@ -12,9 +12,44 @@ module ResourceTools::ResourceTableMigration
   end
   private :has_column?
 
+  # Ensures that changes to the normal tables affect the current ones too, as well as ensuring that the
+  # triggers remain in step too.
   def change_resource_table(name, options = {}, &block)
     change_table(name, options, &block)
     change_table("current_#{name}", options, &block)
+    maintain_currency_triggers(name)
   end
   private :change_resource_table
+
+  # Ensures that the triggers that maintain the current tables are instep with the table definition.
+  #
+  # The trigger created is intended to insert all new values into the current table.  Originally this
+  # did a query back into the normal table, but that caused deadlocks, so here we automatically
+  # generate the column names.  Because this is called from change_resource_table we are ensured that
+  # modifications to the resource tables will maintain the trigger appropriately.
+  def maintain_currency_triggers(table)
+    current_table = "current_#{table}"
+    columns = connection.columns(current_table).map(&:name)
+
+    quoted_columns = columns.map { |n| quote_table_name(n) }.join(',')
+    new_values     = columns.map { |n| "NEW.#{n}" }.join(',')
+
+    drop_trigger("maintain_current_#{table}_trigger")
+    after_trigger(
+      %Q{
+        BEGIN
+          IF NEW.current_to IS NULL OR NEW.deleted_at IS NOT NULL THEN BEGIN
+            DELETE FROM #{current_table} WHERE uuid=NEW.uuid;
+          END ; END IF ;
+          IF NEW.current_to IS NULL THEN BEGIN
+            INSERT INTO #{current_table}(#{quoted_columns}) VALUES(#{new_values});
+          END ; END IF ;
+        END
+      },
+      :name  => "maintain_#{current_table}_trigger",
+      :event => :insert,
+      :on    => table
+    )
+  end
+  private :maintain_currency_triggers
 end
