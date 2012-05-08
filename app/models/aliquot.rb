e3acb6571b3f740ea448784fa8c276bd136b92ce
Matthew Denner <md12@sanger.ac.uk>
2012-05-08 16:01:00 +0100

    [fixes #29246221] Remap bait library fields on aliquots

--- app/models/aliquot.rb
+++ app/models/aliquot.rb
@@ -3,6 +3,12 @@ class Aliquot < ActiveRecord::Base
 
   json do
     ignore(:sample_url, :receptacle_url, :project_url, :study_url, :library_url, :tag_url)
-    translate(:id => :internal_id)
+    translate(
+      :id                               => :internal_id,
+      :bait_library_name                => :bait_name,
+      :bait_library_target_species      => :bait_target_species,
+      :bait_library_supplier_identifier => :bait_supplier_identifier,
+      :bait_library_supplier_name       => :bait_supplier_name
+    )
   end
 end
