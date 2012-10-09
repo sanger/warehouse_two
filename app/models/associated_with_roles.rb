a4c05728a427b96612953ebf3f3ecc569134ed3c
Matthew Denner <md12@sanger.ac.uk>
2012-10-09 13:28:41 +0100

    [#34792267] Only update users on new study record

--- app/models/associated_with_roles.rb
+++ app/models/associated_with_roles.rb
@@ -11,7 +11,7 @@ module AssociatedWithRoles
       user_model = Class.new(ActiveRecord::Base) { include AssociatedWithRoles::User }
       user_model.associated_with(base)
       const_set(:User, user_model)
-      after_save :maintain_users, :if => :current?
+      after_create :maintain_users, :if => :current?
     end
   end
 
