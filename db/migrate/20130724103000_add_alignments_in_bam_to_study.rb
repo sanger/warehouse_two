42753e96f6b9468af75d025d945a5896a5e3de2f
James Glover <jg16@sanger.ac.uk>
2013-07-24 12:24:20 +0100

    [#53946071] Migrations and schema changes

--- db/migrate/20130724103000_add_alignments_in_bam_to_study.rb
+++ db/migrate/20130724103000_add_alignments_in_bam_to_study.rb
@@ -1,4 +1,6 @@
 class AddAlignmentsInBamToStudy < ActiveRecord::Migration
+  include ResourceTools::ResourceTableMigration
+
   def change
     change_resource_table(:studies) do |t|
       t.boolean(:alignments_in_bam, :null => false, :default => true)
--- db/migrate/20130724103504_add_separate_y_chromosome.rb
+++ db/migrate/20130724103504_add_separate_y_chromosome.rb
@@ -1,4 +1,6 @@
 class AddSeparateYChromosome < ActiveRecord::Migration
+  include ResourceTools::ResourceTableMigration
+
   def change
     change_resource_table(:studies) do |t|
       t.boolean(:seperate_y_chromosome_data, :null => false, :default => false)
